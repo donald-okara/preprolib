2ddd22dbf98807e659c018a03bcaa58380c97686
donald-okara <donaldokara123@gmail.com>
2023-07-30 10:52:04 +0300

    update eval model1

--- preprolib/myfunctions.py
+++ preprolib/myfunctions.py
@@ -27,23 +27,22 @@ def cat_or_num(dataframe, ignore, numeric_columns, categorical_columns):
 
 def evaluate_model(curr_model, X_test, y_test):
     import pandas as pd
-    from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score
+    from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
     predictions = curr_model.predict(X_test)
-    # Convert the one-hot encoded y_test to a 1-dimensional array
+
+    #Convert the one-hot encoded y_test to a 1-dimensional array
     y_test_1d = y_test.argmax(axis=1)
 
-    # Calculate the necessary metrics
+    #Calculate the necessary metrics
     accuracy = accuracy_score(y_test_1d, predictions)
     precision = precision_score(y_test_1d, predictions, average='weighted')
     recall = recall_score(y_test_1d, predictions, average='weighted')
     f1 = f1_score(y_test_1d, predictions, average='weighted')
 
-    # Print the evaluation metrics
+    #Print the results for the current model
     print("Model Evaluation:")
     print("Accuracy:", accuracy)
     print("Precision:", precision)
     print("Recall:", recall)
     print("F1-score:", f1)
-
-
