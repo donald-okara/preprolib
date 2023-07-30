3130436cc67c587d29c9fdab3bb7edef207d47d2
donald-okara <donaldokara123@gmail.com>
2023-07-30 09:48:54 +0300

    update evaluate_model function 1

--- preprolib/myfunctions.py
+++ preprolib/myfunctions.py
@@ -24,23 +24,25 @@ def cat_or_num(dataframe, ignore, numeric_columns, categorical_columns):
                 print("The variable categorical_columns list is not present.")
                 print("Please make sure it is present before using it.")
 
+
 def evaluate_model(curr_model, X_test, y_test):
     import pandas as pd
-    from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score
-    
+    from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
     predictions = curr_model.predict(X_test)
+
     # Convert the one-hot encoded y_test to a 1-dimensional array
     y_test_1d = y_test.argmax(axis=1)
-    
+
     # Calculate the necessary metrics
     accuracy = accuracy_score(y_test_1d, predictions)
     precision = precision_score(y_test_1d, predictions, average='weighted')
     recall = recall_score(y_test_1d, predictions, average='weighted')
     f1 = f1_score(y_test_1d, predictions, average='weighted')
-    
-    print("predictions")
-    print("accuracy")
-    print("precision")
-    print("recall")
-    print("f1")
 
+    # Print the results for the current model
+    print("Model Evaluation:")
+    print("Accuracy:", accuracy)
+    print("Precision:", precision)
+    print("Recall:", recall)
+    print("F1-score:", f1)
