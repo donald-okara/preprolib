5a02162b86c23eaa24c7739caec9b3abc0ae7427
donald-okara <donaldokara123@gmail.com>
2023-07-30 10:31:27 +0300

    evaluate_model function reupdated

--- preprolib/myfunctions.py
+++ preprolib/myfunctions.py
@@ -29,18 +29,21 @@ def evaluate_model(curr_model, X_test, y_test):
     from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score
     
     predictions = curr_model.predict(X_test)
+    predictions = curr_model.predict(X_test)
+
     # Convert the one-hot encoded y_test to a 1-dimensional array
     y_test_1d = y_test.argmax(axis=1)
-    
+
     # Calculate the necessary metrics
     accuracy = accuracy_score(y_test_1d, predictions)
     precision = precision_score(y_test_1d, predictions, average='weighted')
     recall = recall_score(y_test_1d, predictions, average='weighted')
     f1 = f1_score(y_test_1d, predictions, average='weighted')
-    
-    print("predictions")
-    print("accuracy")
-    print("precision")
-    print("recall")
-    print("f1")
+
+    # Print the results for the current model
+    print("Model Evaluation:")
+    print("Accuracy:", accuracy)
+    print("Precision:", precision)
+    print("Recall:", recall)
+    print("F1-score:", f1)
 
