9d5e0c6c29e4c9077485fab105c5282ba7b964a1
donald-okara <donaldokara123@gmail.com>
2023-07-30 09:30:41 +0300

    update version name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import numpy as np
 
 setup(
     name='preprolib',  # Replace with the name of your library
-    version='1.0.0',  # Replace with the version number of your library
+    version='1.0.1',  # Replace with the version number of your library
     description='This library contains functions for preprocessing data in python',  # Replace with a short description of your library
     author='Donald Okara Isoe',  # Replace with your name
     packages=['preprolib'],  # Replace 'mylibrary' with the actual package name
